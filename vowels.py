09b35573036d253296aa015640cc55ec3a698f05
avinashelon <46994130+avinashelon@users.noreply.github.com>
2019-01-27 14:47:59 +0530

    vowels.py

--- vowels.py
+++ vowels.py
@@ -1,8 +1,8 @@
-l = input("Input a letter of the alphabet: ")
-
-if l in ('a', 'e', 'i', 'o', 'u'):
-	print("%s is a vowel." % l)
-elif l == 'y':
-	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+n = input("Input a letter of the alphabet: ")
+if n in ('a','e','i','o','u'):
+	print("%s is a vowel." % n)
+elif n =='y':
+	print("sometimes letter y stands for vowel,sometimes stands for consonant.")
 else:
-	print("%s is a consonant." % l) 
+	print("%s is a consonant" % n)
+	
